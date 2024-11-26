e517b896c89a28108b750139b6067ae92cd429d4
Redu99 <maksgasior82@gmail.com>
2024-11-26 10:56:08 +0100

    benc

---
+++ lekcja3/funkcja switch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+int pierwsza_liczba;
+cout <<"podaj pierwszą liczbe:";
+cin >> pierwsza_liczba;
+
+clear druga_liczba;
+cout << "podaj drugą liczbe:";
+cin >> druga_liczba;
+
+clear znakdzialania
+cout << "podaj znak działania (*, +, /, -):"
+
+switch (znakdzialania){
+	case'-':
+	wynik = pierwszaliczba - drugaliczba
+    cout << "Wynik:" << wynik <<endl;
+    break; 
+    case '-':
+    wynik = pierwszaliczba + drugaliczba
+    cout << "Wynik:" << wynik << endl;
+    break;
+    case '-':
+    wynik = pierwszaliczba * drugaliczba
+    cout << "Wynik:" << wynik << endl;
+    break;
+    case '-':
+    wynik = pierwszaliczba / drugaliczba 
+	cout << "Wynik:" << wynik << endl;
+	break;	
+    
+	
+	
+}
+
+
